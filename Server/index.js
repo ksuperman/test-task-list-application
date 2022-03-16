e80c1d4fd3e59c40abc7ffcdf6b5ab548bb79e07
Rakshith Hatwar <rakshith_hatwar@intuit.com>
2022-03-15 23:21:20 -0700

    feat(server): Send HTML Reponse

--- Server/index.js
+++ Server/index.js
@@ -4,9 +4,22 @@ const PORT = process.env.PORT || 3000;
 
 const requestListener = (req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Content-Type', 'text/html; charset=UTF-8');
   const name = process.env.NAME || 'Akshatha Hebbar';
-  res.end(`{"name": "${name}"}`)
+  res.end(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>A simple HTML document</title>
+  	<style>
+      .text {
+        color: red;
+      }
+  </style>
+</head>
+<body>
+    <p class="text">Hello ${name}!<p>
+</body>
+</html>`)
 }
 
 const server = http.createServer(requestListener)
